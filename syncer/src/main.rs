a3196711eeee0b78db60f9dfe20385341d5d2960
lmyslinski <lukmyslinski@gmail.com>
2022-12-02 15:49:55 +0100

    Sleep env var added

--- syncer/src/main.rs
+++ syncer/src/main.rs
@@ -5,9 +5,34 @@ use chrono::Local;
 use env_logger::Builder;
 use log::LevelFilter;
 
+
+
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    configure_logging();
 
+    let time: u64 = env::var("SLEEP_DURATION").unwrap_or("5".to_string()).parse().unwrap();
+    let sleep_duration = time::Duration::from_secs(time);
+    let stakeboard_host = env::var("STAKEBOARD_HOST").unwrap();    
+    
+    loop {
+        log::info!("Calling {}", stakeboard_host);
+        let resp = reqwest::get(format!("{stakeboard_host}/api/sync"))
+            .await?;
+
+
+        //     .json::<HashMap<String, String>>()
+        //     .await?;
+        // log::info!("{:#?}", resp);
+
+        log::info!("Sleeping for {:#?} seconds", sleep_duration);
+        thread::sleep(sleep_duration);
+    }
+}
+
+
+fn configure_logging() {
     Builder::new()
     .format(|buf, record| {
         writeln!(buf,
@@ -19,21 +44,4 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     })
     .filter(None, LevelFilter::Info)
     .init();
-
-    let stakeboard_host = env::var("STAKEBOARD_HOST").unwrap();
-
-    
-    let sleep_duration = time::Duration::from_secs(5);
-    loop {
-        log::info!("Sleeping for {:#?} seconds", sleep_duration);
-        thread::sleep(sleep_duration);
-        log::info!("Calling {}", stakeboard_host);
-        let resp = reqwest::get(format!("{stakeboard_host}/api/sync"))
-            .await?;
-
-            
-        //     .json::<HashMap<String, String>>()
-        //     .await?;
-        // log::info!("{:#?}", resp);
-    }
 }
