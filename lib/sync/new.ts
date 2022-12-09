8a7210d667e7164e23457bb428013638d26334c5
lmyslinski <lukasz.myslinski@datarobot.com>
2022-12-09 13:20:28 +0100

    Add missing logic for new tx sync

--- lib/sync/new.ts
+++ lib/sync/new.ts
@@ -1,7 +1,7 @@
 import Redis from "ioredis";
 import _ from "lodash";
 import { NextApiResponse } from "next";
-import { TX_COLLECTION, LAST_TX_TIMESTAMP } from "../const";
+import { TX_COLLECTION, LAST_TX_TIMESTAMP, CURSOR } from "../const";
 import { CosmosTxResponse } from "../types";
 import { getTx } from "../unchained";
 
@@ -21,6 +21,7 @@ export const runNewSync = async (
   
     if (latestTx != Number(lastTxTimestamp)) {
       console.log("Timestamps differ, getting missing transaction data");
+      await saveCurrentResponse(redis, unchainedTxResponse, lastTxTimestamp)
       loadNewTx(redis, unchainedTxResponse, lastTxTimestamp);
       res
         .status(200)
@@ -30,12 +31,10 @@ export const runNewSync = async (
       res.status(200).json({ message: "Data is up to date" });
     }
   };
-  
-  // Get the latest page
-  // search the page for the lastTimestamp that we have saved
-  // if found, load only the the new data into the db
-  // if not found, load the entire page and move on to the next one
-  const loadNewTx = async (
+
+
+
+  const saveCurrentResponse = async (
     redis: Redis,
     unchainedTxResponse: CosmosTxResponse,
     lastTxTimestamp: number
@@ -56,7 +55,32 @@ export const runNewSync = async (
       console.log("Data has been updated, all Tx up to date");
     } else {
       console.log("last timestamp not found, moving on to the next page");
-      // I'll skip implementing this for now - it will mess up the ordering of the data, 
-      // the likelyhood of us missing an entire page of data with a sync every hour is next to impossible given the current load
+      // save entire page, set cursor for next request
+      const txStrings = unchainedTxResponse.txs.map((tx) => JSON.stringify(tx));
+      await redis.lpush(TX_COLLECTION, ...txStrings);
+      await redis.set(CURSOR, unchainedTxResponse.cursor);
     }
+  }
+
+
+
+
+
+  
+  // Get the latest page
+  // search the page for the lastTimestamp that we have saved
+  // if found, load only the the new data into the db
+  // if not found, load the entire page and move on to the next one
+  const loadNewTx = async (
+    redis: Redis,
+    unchainedTxResponse: CosmosTxResponse,
+    lastTxTimestamp: number
+  ) => {
+    
   };
+
+
+  const loadPagesUntilMatch = () => {
+
+
+  }
